5d54a6f51ae25e6ac149a0f9c7e54b3ba4a48295
Shaun Calagos <scalagos0123@live.com>
2016-06-19 22:34:25 +0800

    added person class

---
+++ app/src/main/java/com/technishaun/teamcdc/Person.java
@@ -0,0 +1,18 @@
+package com.technishaun.teamcdc;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Shaun on 6/19/2016.
+ */
+public class Person {
+
+    private String name;
+    private String birthday;
+    private ArrayList<String> hobbies;
+    private ArrayList<String> interests;
+    private ArrayList<String> movies;
+    private ArrayList<String> songs;
+    private ArrayList<String> games;
+
+}
