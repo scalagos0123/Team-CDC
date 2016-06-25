6e481e9315bd03cb9942b6636eedd1e198bf0b16
Shaun Calagos <scalagos0123@live.com>
2016-06-25 16:02:53 +0800

    * added layoutparams for every listview * changed category list to listview (before was TextViews appended)

--- app/src/main/java/com/technishaun/teamcdc/TeamList.java
+++ app/src/main/java/com/technishaun/teamcdc/TeamList.java
@@ -1,11 +1,13 @@
 package com.technishaun.teamcdc;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
@@ -59,7 +61,6 @@ public class TeamList extends AppCompatActivity {
         Person person2 = new Person("Kat Delfin");
         person2.addGame(new String[]{"Surgeon 3", "Clash Royale"});
         person2.addHobby(new String[]{"Eating", "Sleeping", "Reading", "Watching TV Series"});
-        person2.addMovie(new String[]{"None"});
         person2.addSong(new String[]{"Bring me to Life - Evanescence","Emotion - Carly Rae Jepsen"});
 
         team_details.add(person2);
@@ -90,82 +91,134 @@ public class TeamList extends AppCompatActivity {
             name.setTextAppearance(this, android.R.style.TextAppearance_Large);
             team_layout.addView(name);
 
+//          Games
 
             TextView game_label = new TextView(this);
             game_label.setText("Games: ");
             game_label.setTextAppearance(this, android.R.style.TextAppearance_Medium);
-            team_layout.addView(game_label);
 
-            for (int j = 0; j < team_details.get(i).getGames().size(); j++) {
-                appendedTexts.append(team_details.get(i).getGames().get(j) + ", ");
+//            for (int j = 0; j < team_details.get(i).getGames().size(); j++) {
+//                appendedTexts.append(team_details.get(i).getGames().get(j) + ", ");
+//            }
+
+            ListView list_game = new ListView(this);
+            list_game.setNestedScrollingEnabled(true);
+            list_game.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 300));
+
+            ArrayAdapter<String> list_game_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, team_details.get(i).getGames());
+            list_game.setAdapter(list_game_adapter);
+
+            if (team_details.get(i).getGames().size() > 0) {
+                team_layout.addView(game_label);
+                team_layout.addView(list_game);
             }
 
-            TextView game = new TextView(this);
-            game.setText(appendedTexts);
-            game.setGravity(Gravity.CENTER);
-            team_layout.addView(game);
-            appendedTexts.delete(0, appendedTexts.length());
+//            End of games
+//            Movies
 
             TextView movies_label = new TextView(this);
             movies_label.setText("Movies: ");
             movies_label.setTextAppearance(this, android.R.style.TextAppearance_Medium);
-            team_layout.addView(movies_label);
 
-            for (int j = 0; j < team_details.get(i).getMovies().size(); j++) {
-                appendedTexts.append(team_details.get(i).getMovies().get(j) + ", ");
+            ListView list_movie = new ListView(this);
+            list_movie.setNestedScrollingEnabled(true);
+            list_movie.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 300));
+
+            ArrayAdapter<String> list_movie_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, team_details.get(i).getMovies());
+            list_movie.setAdapter(list_movie_adapter);
+
+            if (team_details.get(i).getMovies().size() > 0) {
+                team_layout.addView(movies_label);
+                team_layout.addView(list_movie);
             }
 
-            TextView movie = new TextView(this);
-            movie.setText(appendedTexts);
-            movie.setGravity(Gravity.CENTER);
-            team_layout.addView(movie);
-            appendedTexts.delete(0, appendedTexts.length());
+//            End of movies
+//            Hobbies
 
             TextView hobbies_label = new TextView(this);
             hobbies_label.setText("Hobbies: ");
             hobbies_label.setTextAppearance(this, android.R.style.TextAppearance_Medium);
-            team_layout.addView(hobbies_label);
 
-            for (int j = 0; j < team_details.get(i).getHobbies().size(); j++) {
-                appendedTexts.append(team_details.get(i).getHobbies().get(j) + ", ");
+//            for (int j = 0; j < team_details.get(i).getHobbies().size(); j++) {
+//                appendedTexts.append(team_details.get(i).getHobbies().get(j) + ", ");
+//            }
+
+//            TextView hobby = new TextView(this);
+//            hobby.setText(appendedTexts);
+//            hobby.setGravity(Gravity.CENTER);
+//            team_layout.addView(hobby);
+//            appendedTexts.delete(0, appendedTexts.length());
+
+            ListView list_hobby = new ListView(this);
+            list_hobby.setNestedScrollingEnabled(true);
+            list_hobby.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 300));
+
+            ArrayAdapter<String> list_hobby_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, team_details.get(i).getHobbies());
+            list_hobby.setAdapter(list_hobby_adapter);
+
+            if (team_details.get(i).getHobbies().size() > 0) {
+                team_layout.addView(hobbies_label);
+                team_layout.addView(list_hobby);
             }
 
-            TextView hobby = new TextView(this);
-            hobby.setText(appendedTexts);
-            hobby.setGravity(Gravity.CENTER);
-            team_layout.addView(hobby);
-            appendedTexts.delete(0, appendedTexts.length());
+//            End of hobbies
+//            Interests
 
             TextView interests_label = new TextView(this);
             interests_label.setText("Interests: ");
             interests_label.setTextAppearance(this, android.R.style.TextAppearance_Medium);
-            team_layout.addView(interests_label);
 
-            for (int j = 0; j < team_details.get(i).getInterests().size(); j++) {
-                appendedTexts.append(team_details.get(i).getInterests().get(j) + ", ");
+//            for (int j = 0; j < team_details.get(i).getInterests().size(); j++) {
+//                appendedTexts.append(team_details.get(i).getInterests().get(j) + ", ");
+//            }
+
+//            TextView interest = new TextView(this);
+//            interest.setText(appendedTexts);
+//            interest.setGravity(Gravity.CENTER);
+//            team_layout.addView(interest);
+//            appendedTexts.delete(0, appendedTexts.length());
+
+            ListView list_interest = new ListView(this);
+            list_interest.setNestedScrollingEnabled(true);
+            list_interest.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 300));
+
+            ArrayAdapter<String> list_interest_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, team_details.get(i).getInterests());
+            list_interest.setAdapter(list_interest_adapter);
+
+            if (team_details.get(i).getInterests().size() > 0) {
+                team_layout.addView(interests_label);
+                team_layout.addView(list_interest);
             }
 
-            TextView interest = new TextView(this);
-            interest.setText(appendedTexts);
-            interest.setGravity(Gravity.CENTER);
-            team_layout.addView(interest);
-            appendedTexts.delete(0, appendedTexts.length());
+//            End of interests
+//            Songs
 
             TextView songs_label = new TextView(this);
             songs_label.setText("Songs: ");
             songs_label.setTextAppearance(this, android.R.style.TextAppearance_Medium);
-            team_layout.addView(songs_label);
 
-            for (int j = 0; j < team_details.get(i).getSongs().size(); j++) {
-                appendedTexts.append(team_details.get(i).getSongs().get(j) + ", ");
-            }
 
-            TextView song = new TextView(this);
-            song.setText(appendedTexts);
-            song.setGravity(Gravity.CENTER);
-            team_layout.addView(song);
-            appendedTexts.delete(0, appendedTexts.length());
+//            for (int j = 0; j < team_details.get(i).getSongs().size(); j++) {
+//                appendedTexts.append(team_details.get(i).getSongs().get(j) + ", ");
+//            }
 //
+//            TextView song = new TextView(this);
+//            song.setText(appendedTexts);
+//            song.setGravity(Gravity.CENTER);
+//            team_layout.addView(song);
+//            appendedTexts.delete(0, appendedTexts.length());
+
+            ListView list_song = new ListView(this);
+            list_song.setNestedScrollingEnabled(true);
+            list_song.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 300));
+
+            ArrayAdapter<String> list_song_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, team_details.get(i).getSongs());
+            list_song.setAdapter(list_movie_adapter);
+
+            if (team_details.get(i).getInterests().size() > 0) {
+                team_layout.addView(songs_label);
+                team_layout.addView(list_song);
+            }
         }
 
     }
